2e7b676bb70a12b3b871f06323939b3b951d2381
Qasim <mq12113@gmail.com>
2024-05-02 16:34:02 +0100

    complete project

--- app.js
+++ app.js
@@ -38,7 +38,47 @@ const topLink = document.querySelector(".top-link")
    } else {
       navbar.classList.remove('fixed-nav')
    }
- })
+
+
+// move to top
+ if (scrollHeight > 500) {
+   topLink.classList.add("show-link");
+ } else {
+   topLink.classList.remove("show-link")
+ }
+})
+
 
 
  // **************** smooth scroll ****************
+
+ const scrollLinks = document.querySelectorAll(".scroll-link")
+ scrollLinks.forEach(function(link) {
+   link.addEventListener("click", function(e) {
+      // prevent default
+      e.preventDefault()
+
+      const id = e.currentTarget.getAttribute('href').slice(1)
+      const element = document.getElementById(id);
+// calculate the height
+      const navHeight = navbar.getBoundingClientRect().height
+      const containerHeight = linksContainer.getBoundingClientRect().height
+
+      const fixedNav = navbar.classList.contains("fixed-nav")      
+      let position = element.offsetTop - navHeight;
+
+      if(!fixedNav){
+         position =  position - navHeight;
+      }
+      if(navHeight > 82) {
+         position = position + containerHeight;
+
+      }
+
+window.scrollTo({
+   left: 0,
+   top: position,
+   });
+   linksContainer.style.height = 0;
+   });
+});
